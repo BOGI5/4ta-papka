58dc74aec363865f86023bb134697d478b9d51b6
stoyanov787 <98825069+stoyanov787@users.noreply.github.com>
2024-03-14 14:25:39 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_login import LoginManager
-from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 
-from config import Config
-
 app = Flask(__name__)
-app.config.from_object(Config)
+USERNAME, PASSWORD, HOST, DATABASE_NAME = "MyUser", "MyPassword", "localhost", "Users"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
+)
+app.config["SECRET_KEY"] = "MySecretKey"
+
 db = SQLAlchemy(app)
-mail = Mail(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
