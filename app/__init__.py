f5d72a5985725cf108cf9da86cab50a8241b7a63
BOGI5 <80194103+BOGI5@users.noreply.github.com>
2024-03-15 12:35:04 +0200

    Config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,10 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
-USERNAME, PASSWORD, HOST, DATABASE_NAME = "MyUser", "MyPassword", "localhost", "Users"
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
-)
-app.config["SECRET_KEY"] = "MySecretKey"
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
